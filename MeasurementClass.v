9eea58a5e4669b4a6c6115ccc2050fc149926c7d
Perry Alexander <palexand@ku.edu>
2022-03-17 18:22:46 -0500

    added preorder; proved some orderings

--- MeasurementClass.v
+++ MeasurementClass.v
@@ -56,16 +56,16 @@ Inductive m : relation Obj :=
 Inductive c : relation Obj :=
 | o1o2 : c o2 o1.
 
-(** Define [MeasurementObj] to be an isntance of [Measurement] over [RTM], [m]
+(** Define [MeasurementSys] to be an isntance of [Measurement] over [RTM], [m]
   and [c]. *)
 
-Instance MeasurementObj: (Measurement RTM m c).
+Instance MeasurementSys: (Measurement RTM m c).
 
 (** Must now prove that all the properties hold for this system.  Coq makes
  this happen automatically. [constructor] breaks up the record into its
  constituent parts.  If a value does not exist for a field, then the prover
  is called to create one. Each of the following produce one proof value for
- each of the 7 theorem types. *)
+ each of the theorem types defined in [Measurement]. *)
 
 Proof.
   constructor.
@@ -106,15 +106,15 @@ Qed.
 
 (** Check the type of [distinct_rtm] as an example *)
 
-Check MeasurementObj.(distinct_rtm).
+Check MeasurementSys.(distinct_rtm).
 
 (** Check the type of [no_depends_rtm_c] as an example *)
 
-Check MeasurementObj.(no_depends_rtm_c).
+Check MeasurementSys.(no_depends_rtm_c).
 
-(** Define an event system that allows specifying instances in time of
+(** Define an event system that allows specifying instances of
  attestation startup, measurement, corruption, and repair. Parameterized
- over [Obj] to allow different object types. *)
+ over [O] to allow different object types. *)
 
 Inductive event(O:Type):Type :=
 | att_start : (event O)
@@ -125,7 +125,7 @@ Inductive event(O:Type):Type :=
 (** A simple even system defined over the inductive type [Obj] *)
 Definition EventObj := (event Obj).
 
-(** A silly transition function from event to event.  Places a partial order
+(** A silly relation that  Places a partial order
  on events.  [att_start] preceds [RTM] measurement of [o1] and [o2].
  Measurement of [o3] by [o2] occurs after measurement of [o2]. *)
 
@@ -134,7 +134,7 @@ Inductive e: (relation EventObj) :=
 | e2 : e (att_start _) (ms _ RTM o2)
 | e3 : e (ms _ RTM o1) (ms _ o1 o3).
 
-Instance ordered_e : (pset eq e).
+Hint Constructors e : core.
 
 (* Define transitive reflexive closure of a relation *)
 Inductive trc{A}(R : (relation A)): A -> A -> Prop :=
@@ -142,33 +142,73 @@ Inductive trc{A}(R : (relation A)): A -> A -> Prop :=
 | TrcFront : forall x y z,
     R x y -> trc R y z -> trc R x z.
 
+Hint Constructors trc : core.
+
+Instance ordered_e : (preset eq (trc e)).
+Proof.
+  constructor.
+  reflexivity.
+
+  (* Shockingly, the transitive reflexive closure of e is in fact transitive
+     and reflexive. *)
+
+  intros x y; intros H; subst; reflexivity.
+
+  intros x y z; intros H1 H2; subst; reflexivity.
+  
+  intros x y; intros H; subst; apply TrcRefl.
+
+  intros x y z; intros H1 H2.
+  induction H1. assumption.
+  eapply TrcFront. apply H. apply IHtrc. apply H2.
+Qed.
+
 (* An event system is an event ordering defined by [delta] and sequences
  of events defined by [traces]. *)
 Class eventSystem(Event:Type) := {
-    delta: (relation Event);
-    leq := (trc delta)
+    leq0: (relation Event);
+    leq := (trc leq0)
     }.
 
 (* A concrete event system defined over [EventObj] using [e] as the value
  for [delta].  Nothing to prove here. *)
-Instance eventSystemObj: (eventSystem EventObj) := {delta := e}.
+Instance eventSystemObj: (eventSystem EventObj) := {leq0 := e}.
 
-Example ex0: traces (att_start _) (ms _ RTM o1).
+Example ex0: leq (att_start _) (ms _ RTM o1).
 Proof.
-  unfold traces.
-  unfold delta.
+  unfold leq,leq0.
   simpl. eapply TrcFront.
   apply e1.
   apply TrcRefl.
 Qed.
 
-Example ex1: traces (att_start _) (ms _ RTM o3).
-Proof.
-  unfold traces; unfold delta.
-  simpl. eapply TrcFront.
-  eapply e1.
-  eapply TrcFront.
-  eapply e3.
-  eapply TrcFront
-  apply e3.
+(* Prove that [att_start] occurs before [o1] measures [o3] *)
 
+Example ex1: leq (att_start _) (ms _ o1 o3).
+Proof.
+  unfold leq, leq0; simpl.
+  eauto.
+Qed.
+
+(* Prove that [RTM] measures [o1] before [o1] measures [o3] *)
+
+Example ex2: leq (ms _ RTM o1) (ms _ o1 o3).
+Proof.
+  unfold leq, leq0; simpl.
+  eauto.
+Qed.
+
+(* Prove that [RTM] measuring [o2] and [RTM] measuring [o1] are not ordered
+ events. *)
+
+Example ex3: ~ (leq (ms _ RTM o1) (ms _ RTM o2)) /\ ~(leq (ms _ RTM o2) (ms _ RTM o1)).
+Proof.
+  unfold not,leq,leq0; simpl; split; intros.
+  inversion H; subst; destruct y.
+  inversion H0.
+  inversion H0; subst. inversion H1; subst. inversion H2. inversion H1. inversion H2.
+  inversion H0.
+  inversion H; subst; destruct y.
+  inversion H0.
+  inversion H0; subst. inversion H1; subst. inversion H2. inversion H1. inversion H2.
+Qed.
--- Pset.v
+++ Pset.v
@@ -6,12 +6,21 @@
 
   
   Class pset{A:Type}(eq:A->A->Prop)(order:A->A->Prop):Type :=
-    { Eq_refl : forall x, (eq x x);
-      Eq_sym : forall x y, (eq x y)->(eq y x);
-      Eq_trans: forall x y z, (eq x y)->(eq y z)->(eq x z);
+    { eq_refl : forall x, (eq x x);
+      eq_sym : forall x y, (eq x y)->(eq y x);
+      eq_trans: forall x y z, (eq x y)->(eq y z)->(eq x z);
 
-      Order_refl : forall x y, eq x y -> order x y;
-      Order_antisym: forall x y, order x y -> order y x -> order x y;
-      Order_trans : forall x y z, order x y -> order y z -> order x z
+      order_refl : forall x y, eq x y -> order x y;
+      order_antisym: forall x y, order x y -> order y x -> eq x y;
+      order_trans : forall x y z, order x y -> order y z -> order x z
+    }.
+
+    Class preset{A:Type}(eq:A->A->Prop)(order:A->A->Prop):Type :=
+    { pre_eq_refl : forall x, (eq x x);
+      pre_eq_sym : forall x y, (eq x y)->(eq y x);
+      pre_eq_trans: forall x y z, (eq x y)->(eq y z)->(eq x z);
+
+      pre_order_refl : forall x y, eq x y -> order x y;
+      pre_order_trans : forall x y z, order x y -> order y z -> order x z
     }.
 
